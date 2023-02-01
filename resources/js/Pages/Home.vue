ee0d17ea04c764c66f9a3717e351e9152b63e4e6
Javier Roman Minguez <javierroman@javierroman.com>
2023-02-01 17:27:18 +0100

    Install laravel, inertia and vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <Nav />
+    <h1>Home</h1>    
+</template>
+
+<script>
+    import Nav from "../Shared/Nav.vue"
+    export default {
+        components: { Nav }
+    }
+</script>
