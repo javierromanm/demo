ddf33567c27f2cbc8fd5541555935920a66d97dc
Javier Roman Minguez <javierroman@javierroman.com>
2023-02-08 13:11:34 +0100

    Include username in share inertia middleware

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,14 +1,22 @@
 <script>
     import Nav from "./Nav.vue"    
     export default {
-        components: { Nav }
+        components: { Nav },
+        computed: {
+            username(){
+                return this.$page.props.auth.user.username
+            }          
+        }
     }
 </script>
 
 <template>
     <section class="bg-gray-200 p-6">
         <header class="flex justify-between">
-            <h1 class="text-3xl">My App</h1>
+            <div class="flex items-center">
+                <h1 class="text-3xl">My App</h1>
+                <p class="ml-6 text-sm">Welcome {{ username }}</p>
+            </div>
             <Nav />          
         </header>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return inertia('Home');
+    return inertia('Home', [
+        'username' => "johndoe"
+    ]);
 });
 
 Route::get('/users', function () {
